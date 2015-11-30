5a037e17fe8f2680cd0a63bdfb73c3ee7e2f445e
Quentin Roy <quentin@quentinroy.fr>
2015-11-30 18:38:22 +0100

    Module content test.

--- test/test.js
+++ test/test.js
@@ -2,6 +2,20 @@
 import test from "tape";
 import tie from "../lib";
 
+test("Module content", (assert) => {
+    const expectedContent = ["Constraint", "liven"];
+    const content = Object.keys(tie);
+    expectedContent.forEach((k) => {
+        assert.notOk(content.indexOf(k) < 0,
+            `${k} is integrated.`
+        )
+    });
+    content.filter(k => expectedContent.indexOf(k) < 0).forEach((k) => {
+        assert.fail(`${k} is not supposed to be integrated.`);
+    });
+    assert.end();
+});
+
 test("Constraint ties", (assert) => {
 	var x = tie(2);
     assert.equal(x.get(), 2,
