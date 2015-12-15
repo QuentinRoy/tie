9243f01202f7667f74b5f30096568317b61c4497
Quentin Roy <quentin@quentinroy.fr>
2015-12-15 18:59:16 +0100

    New eye demo.

--- demos/eyes/eyes.js
+++ demos/eyes/eyes.js
@@ -1,51 +1,69 @@
+function createEye(mainDiv, eyeRadius, pupilRadius, focusPosition, bigPupilColor){
+    // create the DOM
+    var eyeDiv = document.createElement('div');
+    var pupilDiv = document.createElement('pupil');
+    eyeDiv.appendChild(pupilDiv);
+    mainDiv.appendChild(eyeDiv);
+
+    // retrieve eye position and manually update it when necessary
+    var eyePos = tie(function() { return { left: eyeDiv.offsetLeft, top: eyeDiv.offsetTop }; });
+    eyeRadius.onMayHaveChanged(eyePos.invalidate.bind(eyePos));
+
+    // make sure the pupil cannot be bigger than the eye
+    pupilRadius = tie.min(pupilRadius, eyeRadius)
+
+    // calculate pupil position
+    var dx = eyePos.prop("left").add(eyeRadius).sub(focusPosition.prop('x'));
+    var dy = eyePos.prop("top").add(eyeRadius).sub(focusPosition.prop('y'));
+    var d  = tie.min(
+        eyeRadius.sub(pupilRadius),
+        tie.sum(dx.pow(2), dy.pow(2)).sqrt()
+    );
+    var angle = tie.atan2(dy, dx);
+    var pupilLeft = eyeRadius.sub(pupilRadius, d.mul(angle.cos()));
+    var pupilTop  = eyeRadius.sub(pupilRadius, d.mul(angle.sin()));
+
+    var pupilColor = eyeRadius.lte(pupilRadius).ifElse(bigPupilColor, "black");
+
+    // set the style
+    tie.bindStyle(eyeDiv, {
+        "position": "relative",
+        "display": "inline-block",
+        "margin": "5px 10px",
+        "border-radius": "100%",
+        "border-color": "black",
+        "border-style": "solid",
+        "height": eyeRadius.mul(2).add("px"),
+        "width": eyeRadius.mul(2).add("px")
+    });
+    tie.bindStyle(pupilDiv, {
+        "position": "absolute",
+        "border-radius": "100%",
+        "background-color": pupilColor,
+        "left": pupilLeft.add("px"),
+        "top": pupilTop.add("px"),
+        "height": pupilRadius.mul(2).add("px"),
+        "width": pupilRadius.mul(2).add("px")
+    });
+}
+
 window.addEventListener("load", function(){
+
     var mousePosition = tie({x: 0, y: 0});
     document.addEventListener("mousemove", function(evt){
         mousePosition.set({ x: evt.pageX, y: evt.pageY });
     });
 
     var eyeSizeSlider = document.querySelector("#eyes-size");
-    var pupilSizeSlider = document.querySelector("#pupil-size");
-    var eyeBorder = tie(function(){
-        var eye = document.querySelector(".eye");
-        return parseInt(getComputedStyle(eye)["border-width"]);
-    });
-
+    var pupilSizeSlider = document.querySelector("#pupils-size");
     var eyeSliderVal = tie(function(){ return eyeSizeSlider.value });
     var pupilSliderVal = tie(function(){ return pupilSizeSlider.value });
     eyeSizeSlider.addEventListener("input", eyeSliderVal.invalidate.bind(eyeSliderVal));
     pupilSizeSlider.addEventListener("input", pupilSliderVal.invalidate.bind(pupilSliderVal));
 
-    var eyeRadius = tie(eyeSliderVal).parseFloat();
-    var pupilRadius = tie.min(
-        pupilSliderVal.parseFloat(), eyeRadius
-    );
-    var pupilClass = eyeRadius.lte(pupilRadius).ifElse("red");
+    var eyeRadius = eyeSliderVal.parseFloat();
+    var pupilRadius = pupilSliderVal.parseFloat();
 
-    ['left', 'right'].forEach(function(side){
-        var eye = document.querySelector(".eye." + side);
-        var pupil  = eye.querySelector(".pupil");
-        var eyePos = tie(function() { return { left: eye.offsetLeft, top: eye.offsetTop }; });
-        eyeRadius.onMayHaveChanged(eyePos.invalidate.bind(eyePos));
-        var dx = eyePos.prop("left").add(eyeRadius).sub(mousePosition.prop('x'));
-        var dy = eyePos.prop("top").add(eyeRadius).sub(mousePosition.prop('y'));
-        var d  = tie.min(
-            eyeRadius.sub(pupilRadius),
-            tie.sum(dx.pow(2), dy.pow(2)).sqrt()
-        );
-        var angle   = tie.atan2(dy, dx);
-        var eyeLeft = eyeRadius.sub(pupilRadius, d.mul(angle.cos()));
-        var eyeTop  = eyeRadius.sub(pupilRadius, d.mul(angle.sin()));
-        tie.bindStyle(eye, {
-            height: eyeRadius.mul(2).add("px"),
-            width: eyeRadius.mul(2).add("px")
-        });
-        tie.bindStyle(pupil, {
-            left: eyeLeft.add("px"),
-            top: eyeTop.add("px"),
-            height: pupilRadius.mul(2).add("px"),
-            width: pupilRadius.mul(2).add("px")
-        });
-        tie.bindClass(pupil, pupilClass);
-    });
+    createEye(document.querySelector("#eyes"), eyeRadius, pupilRadius, mousePosition, "yellow");
+    createEye(document.querySelector("#eyes"), eyeRadius.mul(0.7), pupilRadius.mul(0.8), mousePosition, "red");
 });
